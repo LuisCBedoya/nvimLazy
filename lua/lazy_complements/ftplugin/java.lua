be54c1d9673be8a09dffcb5d54c550c16497ff72
Luis Carlos R Bedoya Andocilla <90487955+LuisCBedoya@users.noreply.github.com>
2024-01-18 21:29:22 -0500

    Update java.lua

--- lua/lazy_complements/ftplugin/java.lua
+++ lua/lazy_complements/ftplugin/java.lua
@@ -1,4 +1,5 @@
 local config = {
+  -- '/usr/lib/jvm/java-21-jdk/bin/java',
   cmd = { '/home/luc/.local/share/nvim/mason/bin/jdtls' },
   root_dir = vim.fs.dirname(
     vim.fs.find({ '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' }, { upward = true })[1]
